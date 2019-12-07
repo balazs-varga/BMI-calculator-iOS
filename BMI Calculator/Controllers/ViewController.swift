8c712a3ef963f0466680a72e69c15db6f68d8f85
balazs-varga <balazsvarga91@gmail.com>
2019-12-07 15:57:58 +0100

    calculate bmi and create second view controller

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -2,11 +2,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.2f", sender.value) + "m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightLabel.text = String(Int(sender.value)) + "Kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let bmi = weight / pow(height, 2)
+    }
 }
 
